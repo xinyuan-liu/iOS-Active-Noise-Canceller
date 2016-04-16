beeeea500990c8f80a9aa3f57f8ea87c4ff996f0
Xinyuan Liu <lxy_pku@qq.com>
2016-04-17 01:57:30 +0800

    Initial Commit

---
+++ Active-Noise-Canceller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Active-Noise-Canceller
+//
+//  Created by liuxinyuan on 16/4/17.
+//  Copyright © 2016年 liuxinyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
