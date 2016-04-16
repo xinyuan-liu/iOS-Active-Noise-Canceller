c5fe7a0849d19469e4a7c64e6445b5809321cefb
Xinyuan Liu <lxy_pku@qq.com>
2016-04-17 02:13:57 +0800

    commit

--- Active-Noise-Canceller/ViewController.swift
+++ Active-Noise-Canceller/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var engine = AVAudioEngine()
+    //var distortion = AVAudioUnitDistortion()
+    //var reverb = AVAudioUnitReverb()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup AVAudioSession
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord)
+            
+            let ioBufferDuration = 128.0 / 44100.0
+            
+            try AVAudioSession.sharedInstance().setPreferredIOBufferDuration(ioBufferDuration)
+            
+        } catch {
+            assertionFailure("AVAudioSession setup error: \(error)")
+        }
+        
+        
+        // Setup engine and node instances
+        assert(engine.inputNode != nil)
+        let input = engine.inputNode!
+        let output = engine.outputNode
+        let format = input.inputFormatForBus(0)
+        
+        
+        //distortion.loadFactoryPreset(.DrumsBitBrush)
+        //distortion.preGain = 4.0
+        //engine.attachNode(distortion)
+        
+        //reverb.loadFactoryPreset(.MediumChamber)
+        //reverb.wetDryMix = 80
+        //engine.attachNode(reverb)
+        
+        // Connect nodes
+        engine.connect(input, to: output, format: format)
+        
+        //engine.connect(distortion, to: reverb, format: format)
+        
+        //engine.connect(reverb, to: output, format: format)
+        
+        
+        // Start engine
+        do {
+            try engine.start()
+        } catch {
+            assertionFailure("AVAudioEngine start error: \(error)")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
